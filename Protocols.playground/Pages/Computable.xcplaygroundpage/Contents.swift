3e36a2d5205fd7762284a5b5daa66fa6ccb7fc87
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-27 17:43:15 -0600

    compute function added to all classes

--- Protocols.playground/Pages/Computable.xcplaygroundpage/Contents.swift
+++ Protocols.playground/Pages/Computable.xcplaygroundpage/Contents.swift
@@ -14,13 +14,22 @@ class Sum {
 }
 
 class Product {
-    
+    var numbers = [1,2,3,4,5,6,7,8,9]
+    func compute() -> Int {
+        
+    }
 }
 
 class CountCharacters {
-    
+    var string: String = "odinfeoivneflndwcnervbunlinwe;dqwmid"
+    func compute() -> Int {
+        
+    }
 }
 
 class Random {
-
+    var string: String = "odinfeoivneflndwcnervbunlinwe;dqwmid"
+    func compute() -> Int {
+        
+    }
 }
