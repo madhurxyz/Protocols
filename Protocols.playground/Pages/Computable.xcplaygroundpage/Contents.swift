9093dbb2115c60872b727660d717750628caa50e
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-27 17:47:50 -0600

    all classes assigned Computable protocol

--- Protocols.playground/Pages/Computable.xcplaygroundpage/Contents.swift
+++ Protocols.playground/Pages/Computable.xcplaygroundpage/Contents.swift
@@ -18,7 +18,7 @@ class Sum: Computable {
     }
 }
 
-class Product {
+class Product: Computable {
     var numbers = [1,2,3,4,5,6,7,8,9]
     func compute() -> Int {
         var myProduct = 0
@@ -30,7 +30,7 @@ class Product {
     }
 }
 
-class CountCharacters {
+class CountCharacters: Computable {
     var string: String = "odinfeoivneflndwcnervbunlinwe;dqwmid"
     func compute() -> Int {
         let myString = string.characters.count
@@ -39,7 +39,7 @@ class CountCharacters {
     }
 }
 
-class Random {
+class Random: Computable {
     var string: String = "odinfeoivneflndwcnervbunlinwe;dqwmid"
     func compute() -> Int {
         let myRandom = Int(arc4random())
